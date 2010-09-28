alibaba/tengine

commit 3a6ab18437fe66b44a59c6aedaff570c29717ee0

allow duplicate listen ssl options
git-svn-id: svn://svn.nginx.org/nginx/trunk@3765 73f98a42-aea0-e011-b76d-00259023448c

src/http/ngx_http.c
@@ -1221,6 +1221,9 @@ ngx_http_add_addresses(ngx_conf_t *cf, ngx_http_core_srv_conf_t *cscf,
 #if (NGX_HAVE_UNIX_DOMAIN)
     struct sockaddr_un    *saun;
 #endif
+#if (NGX_HTTP_SSL)
+    ngx_uint_t             ssl;
+#endif
 
     /*
      * we can not compare whole sockaddr struct's as kernel
@@ -1270,6 +1273,10 @@ ngx_http_add_addresses(ngx_conf_t *cf, ngx_http_core_srv_conf_t *cscf,
         /* preserve default_server bit during listen options overwriting */
         default_server = addr[i].opt.default_server;
 
+#if (NGX_HTTP_SSL)
+        ssl = lsopt->ssl || addr[i].opt.set;
+#endif
+
         if (lsopt->set) {
 
             if (addr[i].opt.set) {
@@ -1296,6 +1303,9 @@ ngx_http_add_addresses(ngx_conf_t *cf, ngx_http_core_srv_conf_t *cscf,
         }
 
         addr[i].opt.default_server = default_server;
+#if (NGX_HTTP_SSL)
+        addr[i].opt.ssl = ssl;
+#endif
 
         return NGX_OK;
     }

src/http/ngx_http_core_module.c
@@ -3569,7 +3569,6 @@ ngx_http_core_listen(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 
         if (ngx_strcmp(value[n].data, "ssl") == 0) {
 #if (NGX_HTTP_SSL)
-            lsopt.set = 1;
             lsopt.ssl = 1;
             continue;
 #else

commit 06ef268e128a6e33769d9c544c560f3686cc4b4f

nginx uses SSL mode for a listen socket with any option set, the bug has been introduced in r3765
git-svn-id: svn://svn.nginx.org/nginx/trunk@3769 73f98a42-aea0-e011-b76d-00259023448c

src/http/ngx_http.c
@@ -1274,7 +1274,7 @@ ngx_http_add_addresses(ngx_conf_t *cf, ngx_http_core_srv_conf_t *cscf,
         default_server = addr[i].opt.default_server;
 
 #if (NGX_HTTP_SSL)
-        ssl = lsopt->ssl || addr[i].opt.set;
+        ssl = lsopt->ssl || addr[i].opt.ssl;
 #endif
 
         if (lsopt->set) {