alibaba/tengine

commit 2dfe937dc7b8b49a476cae54b869c3217de1db11

move ngx_strerror_init() at the very start
git-svn-id: svn://svn.nginx.org/nginx/trunk@3799 73f98a42-aea0-e011-b76d-00259023448c

src/core/nginx.c
@@ -203,6 +203,10 @@ main(int argc, char *const *argv)
     ngx_cycle_t      *cycle, init_cycle;
     ngx_core_conf_t  *ccf;
 
+    if (ngx_strerror_init() != NGX_OK) {
+        return 1;
+    }
+
     if (ngx_get_options(argc, argv) != NGX_OK) {
         return 1;
     }
@@ -269,10 +273,6 @@ main(int argc, char *const *argv)
 #endif
 
     ngx_pid = ngx_getpid();
-
-    if (ngx_strerror_init() != NGX_OK) {
-        return 1;
-    }
 
     log = ngx_log_init(ngx_prefix);
     if (log == NULL) {

commit 03b5d4cd2f8829ee926f8119fdddd73bf43bf9b0

Merge of r4223:
FreeBSD's MALLOC_OPTIONS must be set before any malloc() call.
The bug has been introduced in r3799.


git-svn-id: svn://svn.nginx.org/nginx/branches/stable-1.0@4353 73f98a42-aea0-e011-b76d-00259023448c

src/core/nginx.c
@@ -203,6 +203,10 @@ main(int argc, char *const *argv)
     ngx_cycle_t      *cycle, init_cycle;
     ngx_core_conf_t  *ccf;
 
+#if (NGX_FREEBSD)
+    ngx_debug_init();
+#endif
+
     if (ngx_strerror_init() != NGX_OK) {
         return 1;
     }
@@ -259,10 +263,6 @@ main(int argc, char *const *argv)
             return 0;
         }
     }
-
-#if (NGX_FREEBSD)
-    ngx_debug_init();
-#endif
 
     /* TODO */ ngx_max_sockets = -1;
 