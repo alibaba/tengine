42aacaca2d927c2270e4b5e25b629359e8f3b1c0
Weibin Yao <yaoweibin@gmail.com>
2012-07-06 16:49:53 +0800

    merged changes between nginx-1.2.0 and nginx-1.2.2

--- src/os/unix/ngx_process_cycle.c
+++ src/os/unix/ngx_process_cycle.c
@@ -753,6 +753,8 @@ ngx_master_process_exit(ngx_cycle_t *cycle)
 #endif
 
     ngx_exit_cycle.log = &ngx_exit_log;
+    ngx_exit_cycle.files = ngx_cycle->files;
+    ngx_exit_cycle.files_n = ngx_cycle->files_n;
     ngx_cycle = &ngx_exit_cycle;
 
     ngx_destroy_pool(cycle->pool);
@@ -1131,6 +1133,8 @@ ngx_worker_process_exit(ngx_cycle_t *cycle)
 #endif
 
     ngx_exit_cycle.log = &ngx_exit_log;
+    ngx_exit_cycle.files = ngx_cycle->files;
+    ngx_exit_cycle.files_n = ngx_cycle->files_n;
     ngx_cycle = &ngx_exit_cycle;
 
     ngx_destroy_pool(cycle->pool);
