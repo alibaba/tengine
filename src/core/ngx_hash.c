d9c126134b52d6325a5d3760a1cb4b3ddf229ca2
jizhao <fanjizhao@126.com>
2012-09-17 18:01:47 +0800

    Increased the initial sizes of some arrays for reducing the memory footprint when there are up to 40000 virtual servers.

--- src/core/ngx_hash.c
+++ src/core/ngx_hash.c
@@ -791,7 +791,7 @@ ngx_hash_add_key(ngx_hash_keys_arrays_t *ha, ngx_str_t *key, void *value,
         }
 
     } else {
-        if (ngx_array_init(&ha->keys_hash[k], ha->temp_pool, 4,
+        if (ngx_array_init(&ha->keys_hash[k], ha->temp_pool, 8,
                            sizeof(ngx_str_t))
             != NGX_OK)
         {
@@ -846,7 +846,7 @@ wildcard:
             }
 
         } else {
-            if (ngx_array_init(&ha->keys_hash[k], ha->temp_pool, 4,
+            if (ngx_array_init(&ha->keys_hash[k], ha->temp_pool, 8,
                                sizeof(ngx_str_t))
                 != NGX_OK)
             {
