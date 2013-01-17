257d5dd8da3a600113afa0456536d74ecb117760
dinic <x.dinic@gmail.com>
2013-01-17 12:29:58 +0800

    bugfixed

--- src/http/modules/ngx_http_upstream_consistent_hash_module.c
+++ src/http/modules/ngx_http_upstream_consistent_hash_module.c
@@ -740,7 +740,7 @@ ngx_http_upstream_chash_tries(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
     }
 
     value = cf->args->elts;
-    tries = ngx_atoi(value->data, value->len);
+    tries = ngx_atoi(value[1].data, value[1].len);
     if (tries != NGX_ERROR) {
         ucscf->tries = (ngx_uint_t) tries;
     }
