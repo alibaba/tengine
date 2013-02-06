1a0c46f4a926e16711925f4539cd3f0cbc263726
lifeibo <lifeibo382005@gmail.com>
2013-02-06 16:34:48 +0800

    New feature: sysguard suppport memfree.

--- src/os/unix/ngx_solaris.h
+++ src/os/unix/ngx_solaris.h
@@ -9,6 +9,10 @@
 #define _NGX_SOLARIS_H_INCLUDED_
 
 
+#if (NGX_HAVE_GETLOADAVG)
+#include <sys/loadavg.h>
+#endif
+
 ngx_chain_t *ngx_solaris_sendfilev_chain(ngx_connection_t *c, ngx_chain_t *in,
     off_t limit);
 
