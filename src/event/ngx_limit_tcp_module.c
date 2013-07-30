ebdcf104e9c3682fbe52e718e8024347e2ea6331
yzprofile <yzprofiles@gmail.com>
2013-07-30 15:48:32 +0800

    Bugfixed: overflow by concurrent -1

--- src/event/ngx_limit_tcp_module.c
+++ src/event/ngx_limit_tcp_module.c
@@ -1108,10 +1108,15 @@ ngx_limit_tcp_lookup(ngx_connection_t *c, ngx_limit_tcp_ctx_t *ctx,
                            "limit tcp count %ui %ui %p",
                            lr->count, addr.len, addr.data);
 
-            if (ctx->concurrent && lr->count + 1 > ctx->concurrent) {
+            (void) ngx_atomic_fetch_add(&lr->count, 1);
+
+            if (ctx->concurrent && lr->count > ctx->concurrent) {
                 ngx_log_error(NGX_LOG_INFO, c->log, 0,
                               "limit tcp %V over concurrent: %ui",
                               &c->addr_text, lr->count);
+
+                (void) ngx_atomic_fetch_add(&lr->count, -1);
+
                 return NGX_BUSY;
             }
 
@@ -1135,10 +1140,9 @@ ngx_limit_tcp_lookup(ngx_connection_t *c, ngx_limit_tcp_ctx_t *ctx,
             if ((ngx_uint_t) excess > ctx->burst) {
                 ngx_log_error(NGX_LOG_INFO, c->log, 0,
                               "limit %V over rate: %i", &c->addr_text, excess);
+                (void) ngx_atomic_fetch_add(&lr->count, -1);
                 return NGX_BUSY;
             }
-
-            (void) ngx_atomic_fetch_add(&lr->count, 1);
 
             lr->excess = excess;
             lr->last = now;
