eb46ef76868440bbeddf6bde4b618d6726b58279
zhcn381 <mingyan.zc@gmail.com>
2013-10-14 14:30:39 +0800

    nginx-tfs new features and bug-fixs.
    1. fix nginx do not close connection bug
    2. use readv2 if read from start
    3. write failed now retry ns instead of ds
    4. fix do not remove dirty block cache when stat large file bug
    5. fix pread custom file bug when read offset is larger than 2 times of segment's start offset
    6. fix rc timer bug
    7. fix write failed bug when data to write is in memory
    8. fix select cluster bug
    9. add Content-type response header

--- src/http/modules/tfs/ngx_tfs_common.c
+++ src/http/modules/tfs/ngx_tfs_common.c
@@ -202,6 +202,7 @@ ngx_http_tfs_compute_buf_crc(ngx_http_tfs_crc_t *t_crc, ngx_buf_t *b,
                                       (const char *) (b->pos), size);
         t_crc->data_crc = ngx_http_tfs_crc(t_crc->data_crc,
                                            (const char *) (b->pos), size);
+        b->last = b->pos + size;
         return NGX_OK;
     }
 
@@ -834,3 +835,83 @@ ngx_http_tfs_alloc_st(ngx_http_tfs_t *t)
 }
 
 
+ngx_int_t
+ngx_http_tfs_get_content_type(u_char *data, ngx_str_t *type)
+{
+    if (memcmp(data, "GIF", 3) == 0) {
+        ngx_str_set(type, "image/gif");
+        return NGX_OK;
+    }
+
+    if (memcmp(data, "\xff\xd8\xff", 3) == 0) {
+        ngx_str_set(type, "image/jpeg");
+        return NGX_OK;
+    }
+
+    if (memcmp(data, "\x89\x50\x4e\x47\x0d\x0a\x1a\x0a", 8) == 0) {
+        ngx_str_set(type, "image/png");
+        return NGX_OK;
+    }
+
+    if ((memcmp(data, "CWS", 3) == 0)
+              ||(memcmp(data, "FWS", 3) == 0))
+    {
+        ngx_str_set(type, "application/x-shockwave-flash");
+        return NGX_OK;
+    }
+
+    if ((memcmp(data, "BM", 2) == 0)
+              ||(memcmp(data, "BA", 2) == 0)
+              ||(memcmp(data, "CI", 2) == 0)
+              ||(memcmp(data, "CP", 2) == 0)
+              ||(memcmp(data, "IC", 2) == 0)
+              ||(memcmp(data, "PI", 2) == 0))
+    {
+        ngx_str_set(type, "image/bmp");
+        return NGX_OK;
+    }
+
+    if ((memcmp(data, "\115\115\000\052", 4) == 0)
+            ||(memcmp(data, "\111\111\052\000", 4) == 0)
+            ||(memcmp(data, "\115\115\000\053\000\010\000\000", 8) == 0)
+            ||(memcmp(data, "\111\111\053\000\010\000\000\000", 8) == 0))
+    {
+        ngx_str_set(type, "image/tiff");
+        return NGX_OK;
+    }
+
+    return NGX_AGAIN;
+}
+
+
+ngx_int_t
+ngx_chain_add_copy_with_buf(ngx_pool_t *pool, ngx_chain_t **chain, ngx_chain_t *in)
+{
+    ngx_buf_t    *b;
+    ngx_chain_t  *cl, **ll;
+
+    ll = chain;
+    for (cl = *chain; cl; cl = cl->next) {
+        ll = &cl->next;
+    }
+
+    while (in) {
+        b = ngx_alloc_buf(pool);
+        if (b == NULL) {
+            return NGX_ERROR;
+        }
+        ngx_memcpy(b, in->buf, sizeof(ngx_buf_t));
+        cl = ngx_alloc_chain_link(pool);
+        if (cl == NULL) {
+            return NGX_ERROR;
+        }
+        cl->buf = b;
+        *ll = cl;
+        ll = &cl->next;
+        in = in->next;
+    }
+
+    *ll = NULL;
+
+    return NGX_OK;
+}
