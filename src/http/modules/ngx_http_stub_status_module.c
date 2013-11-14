5b54b543792fd6e0a30c1c213063b4aef3f14ea3
静龙 <jinglong.pq@taobao.com>
2013-11-14 10:44:20 +0800

    bugfix: don't record request time in subrequest

--- src/http/modules/ngx_http_stub_status_module.c
+++ src/http/modules/ngx_http_stub_status_module.c
@@ -158,6 +158,10 @@ ngx_http_status_log_handler(ngx_http_request_t *r)
     struct timeval             tv;
     ngx_http_core_loc_conf_t  *clcf;
 
+    if (r != r->main) {
+        return NGX_OK;
+    }
+
     clcf = ngx_http_get_module_loc_conf(r, ngx_http_core_module);
     if (clcf->request_time_cache) {
         tp = ngx_timeofday();
