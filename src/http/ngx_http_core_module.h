5fa7545075f9b287084b4ac98109a7d20187c922
zhcn381 <mingyan.zc@gmail.com>
2013-12-11 15:19:27 +0800

    add expires_by_types wildcard type support e.g. expires_by_types 10y image/*;

--- src/http/ngx_http_core_module.h
+++ src/http/ngx_http_core_module.h
@@ -511,6 +511,7 @@ ngx_int_t ngx_http_core_content_phase(ngx_http_request_t *r,
 
 
 void *ngx_http_test_content_type(ngx_http_request_t *r, ngx_hash_t *types_hash);
+void *ngx_http_test_content_type_wildcard(ngx_http_request_t *r, ngx_hash_t *types_hash);
 ngx_int_t ngx_http_set_content_type(ngx_http_request_t *r);
 void ngx_http_set_exten(ngx_http_request_t *r);
 ngx_int_t ngx_http_set_etag(ngx_http_request_t *r);
