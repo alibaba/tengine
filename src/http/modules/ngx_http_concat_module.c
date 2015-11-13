310b4ff44bb715def8c0222f543553a5a66d8bca
IYism <admin@iyism.com>
2015-11-13 16:37:59 +0800

    Update ngx_http_concat_module.c
    bugfix: x-javascript -> javascript

--- src/http/modules/ngx_http_concat_module.c
+++ src/http/modules/ngx_http_concat_module.c
@@ -30,7 +30,7 @@ static char *ngx_http_concat_merge_loc_conf(ngx_conf_t *cf, void *parent,
 
 
 static ngx_str_t  ngx_http_concat_default_types[] = {
-    ngx_string("application/x-javascript"),
+    ngx_string("application/javascript"),
     ngx_string("text/css"),
     ngx_null_string
 };
