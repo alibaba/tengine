f671d0092d77864047c15f1b3828886aea86ae4f
weiyue <weiyue@taobao.com>
2015-12-15 17:18:13 +0800

    add upstream.name

--- src/http/ngx_http_upstream.c
+++ src/http/ngx_http_upstream.c
@@ -5282,6 +5282,7 @@ ngx_http_upstream_server(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
         goto invalid;
     }
 
+    us->name = u.url;
     us->addrs = u.addrs;
     us->naddrs = u.naddrs;
     us->host = u.host;
