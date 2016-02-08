292b0cdc95af30e8e2d4d9a4ea3a31b991c2fe15
MagicBear <mb@bilibili.com>
2016-02-08 08:40:28 +0800

    configure the build options

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -9,14 +9,20 @@
 #define _NGINX_H_INCLUDED_
 
 
-#define nginx_version      1006002
-#define NGINX_VERSION      "1.6.2"
+#define nginx_version      1007001
+#define NGINX_VERSION      "1.7.1"
 #define NGINX_VER          "nginx/" NGINX_VERSION
 
 #define TENGINE            "Tengine"
 #define tengine_version    2001002
 #define TENGINE_VERSION    "2.1.2"
 #define TENGINE_VER        TENGINE "/" TENGINE_VERSION
+
+#ifdef NGX_BUILD
+#define NGINX_VER_BUILD    NGINX_VER " (" NGX_BUILD ")"
+#else
+#define NGINX_VER_BUILD    NGINX_VER
+#endif
 
 #define NGINX_VAR          "NGINX"
 #define NGX_OLDPID_EXT     ".oldbin"
