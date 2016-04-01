23c0c19f327984f4efbc242abc21178ad4d7eb13
yzprofile <yzprofiles@gmail.com>
2016-04-01 15:28:13 +0800

    fix: do not call ngx_http_upstream_check_delete_dynamic_peer in dyups module

--- src/http/modules/ngx_http_dyups_module.c
+++ src/http/modules/ngx_http_dyups_module.c
@@ -1540,7 +1540,7 @@ ngx_dyups_init_upstream(ngx_http_dyups_srv_conf_t *duscf, ngx_str_t *name,
 
     duscf->dynamic = 1;
     duscf->upstream = uscf;
-    
+
     ngx_memzero(&cf, sizeof(ngx_conf_t));
     cf.module_type = NGX_HTTP_MODULE;
     cf.cmd_type = NGX_HTTP_MAIN_CONF;
@@ -1616,13 +1616,6 @@ ngx_dyups_mark_upstream_delete(ngx_http_dyups_srv_conf_t *duscf)
     us = uscf->servers->elts;
     for (i = 0; i < uscf->servers->nelts; i++) {
         us[i].down = 1;
-
-#if (NGX_HTTP_UPSTREAM_CHECK)
-        if (us[i].addrs) {
-            ngx_http_upstream_check_delete_dynamic_peer(&uscf->host,
-                                                        us[i].addrs);
-        }
-#endif
     }
 
     uscfp[duscf->idx] = &ngx_http_dyups_deleted_upstream;
