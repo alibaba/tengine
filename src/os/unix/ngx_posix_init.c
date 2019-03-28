0e200cbc8ab315c358272ef0dea9f721aec1b346
Allen Zhong <zhongbenli@pingcap.com>
2019-03-28 17:57:14 +0800

    use macro when determining version string

--- src/os/unix/ngx_posix_init.c
+++ src/os/unix/ngx_posix_init.c
@@ -98,7 +98,11 @@ ngx_os_init(ngx_log_t *log)
 void
 ngx_os_status(ngx_log_t *log)
 {
-    ngx_log_error(NGX_LOG_NOTICE, log, 0, TENGINE_VER_BUILD " (" NGINX_VER ")");
+#if (T_NGX_SERVER_INFO)
+    ngx_log_error(NGX_LOG_NOTICE, log, 0, TENGINE_VER_BUILD);
+#else
+    ngx_log_error(NGX_LOG_NOTICE, log, 0, NGINX_VER_BUILD);
+#endif
 
 #ifdef NGX_COMPILER
     ngx_log_error(NGX_LOG_NOTICE, log, 0, "built by " NGX_COMPILER);
