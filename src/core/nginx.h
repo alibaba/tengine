8a9d7eb9877c9ef7157407ac837feb0c02122062
wangfakang <fakangwang@gmail.com>
2019-08-20 13:14:10 +0800

    Modify some files for release 2.3.2 version.

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -14,8 +14,8 @@
 #define NGINX_VER          "nginx/" NGINX_VERSION
 
 #define TENGINE            "Tengine"
-#define tengine_version    2003001
-#define TENGINE_VERSION    "2.3.1"
+#define tengine_version    2003002
+#define TENGINE_VERSION    "2.3.2"
 #define TENGINE_VER        TENGINE "/" TENGINE_VERSION
 
 #ifdef NGX_BUILD
