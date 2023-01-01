6d8b79af24e1fb1d8913413f8819f6ed7593fd80
Xiaochen Wang <wangxiaochen0@gmail.com>
2023-01-01 14:10:33 +0800

    updated to nginx-1.22.1: removed NGX_HAVE_AIO_SENDFILE logic

--- src/event/ngx_event.h
+++ src/event/ngx_event.h
@@ -155,10 +155,6 @@ struct ngx_event_aio_s {
 
     ngx_fd_t                   fd;
 
-#if (NGX_HAVE_AIO_SENDFILE || NGX_COMPAT)
-    ssize_t                  (*preload_handler)(ngx_buf_t *file);
-#endif
-
 #if (NGX_HAVE_EVENTFD)
     int64_t                    res;
 #endif
